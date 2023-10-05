f76b776446b229e4e13996fb61e262ee40cdacbc
ErenYea <drstoneeren@gmail.com>
2023-10-06 01:04:11 +0500

    Completed the initial Design of Website

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center md:lg:xl:gap-y-6  md:lg:xl:py-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center md:lg:xl:gap-y-6  lg:xl:py-8">
           <Link href={"/"}>
             <Image
               src={"/logom.png"}
