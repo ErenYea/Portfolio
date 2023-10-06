e0e3b5165e41d6d59940239b6b8f41693628047d
ErenYea <drstoneeren@gmail.com>
2023-10-06 15:25:49 +0500

    Completed the initial Design of Website

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import { Sora } from "@next/font/google";
-
+import Head from "next/head";
 const sora = Sora({
   subsets: ["latin"],
   variable: "--font-sora",
@@ -13,14 +13,24 @@ import TopLeftImg from "../components/TopLeftImg";
 
 const Layout = ({ children }) => {
   return (
-    <div
-      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
-    >
-      <TopLeftImg />
-      <Nav />
-      <Header />
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>My Portfolio</title>
+        <meta name="description" content="Hamza's Portfolio" />
+        <meta name="keywords" content="Portfolio, Hamza's Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logom.png" />
+        <link rel="apple-touch-icon" href="/logom.png" />
+      </Head>
+      <div
+        className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+      >
+        <TopLeftImg />
+        <Nav />
+        <Header />
+        {children}
+      </div>
+    </>
   );
 };
 
